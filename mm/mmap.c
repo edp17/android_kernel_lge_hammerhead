61574ffead944ca4dc8e862616b9bf128fb08e4d
edp17 <edp17@pm.me>
2026-08-12 09:07:41 +0100

    hammerhead: remove temporary Waydroid MM diagnostics
    Remove the temporary WMMAP, VMAADJ, VMAMERGE and RBDBG instrumentation
    used while diagnosing the Waydroid mmap/rbtree failure.
    
    Restore mm/mmap.c and lib/rbtree.c to their pre-diagnostic state and disable
    CONFIG_DEBUG_VM.
    
    Keep CONFIG_OVERLAY_FS, CONFIG_SW_SYNC_USER and all functional Waydroid
    kernel compatibility changes intact.
    
    Not runtime-tested yet; validation is deferred to the Sailfish OS 5.1
    kernel/droid-hal rebuild.

--- mm/mmap.c
+++ mm/mmap.c
@@ -37,7 +37,6 @@
 #include <asm/mmu_context.h>
 
 #include "internal.h"
-#include <linux/string.h>
 
 #ifndef arch_mmap_check
 #define arch_mmap_check(addr, len, flags)	(0)
@@ -46,22 +45,6 @@
 #ifndef arch_rebalance_pgtables
 #define arch_rebalance_pgtables(addr, len)		(addr)
 #endif
-
-static const char *waydroid_mmap_file_name(struct file *file)
-{
-    if (!file || !file->f_path.dentry)
-	return "anon";
-
-    return (const char *)file->f_path.dentry->d_name.name;
-}
-
-static int waydroid_mmap_dbg_task(void)
-{
-    return !strcmp(current->comm, "sh") ||
-           !strcmp(current->comm, "linker") ||
-           !strcmp(current->comm, "toybox") ||
-           !strcmp(current->comm, "init");
-}
 
 static void unmap_region(struct mm_struct *mm,
 		struct vm_area_struct *vma, struct vm_area_struct *prev,
@@ -518,34 +501,6 @@ int vma_adjust(struct vm_area_struct *vma, unsigned long start,
 	long adjust_next = 0;
 	int remove_next = 0;
 
-    if (unlikely(waydroid_mmap_dbg_task())) {
-	pr_emerg("VMAADJ: enter comm=%s pid=%d vma=%p old=%lx-%lx req=%lx-%lx pgoff=%lx insert=%p prev=%p next=%p next_range=%lx-%lx\n",
-	     current->comm, current->pid,
-	     vma,
-	     vma->vm_start, vma->vm_end,
-	     start, end,
-	     (unsigned long)pgoff,
-	     insert,
-	     vma->vm_prev,
-	     next,
-	     next ? next->vm_start : 0UL,
-	     next ? next->vm_end : 0UL);
-
-	if (unlikely(start > end ||
-	         vma->vm_start > vma->vm_end ||
-	         (next && next->vm_start > next->vm_end))) {
-	    pr_emerg("VMAADJ-BAD-ENTER: vma=%p old=%lx-%lx req=%lx-%lx next=%p next_range=%lx-%lx\n",
-		 vma,
-		 vma->vm_start, vma->vm_end,
-		 start, end,
-		 next,
-		 next ? next->vm_start : 0UL,
-		 next ? next->vm_end : 0UL);
-	    dump_stack();
-	    BUG();
-	}
-    }
-
 	if (next && !insert) {
 		struct vm_area_struct *exporter = NULL;
 
@@ -632,30 +587,6 @@ again:			remove_next = 1 + (end > next->vm_end);
 		next->vm_start += adjust_next << PAGE_SHIFT;
 		next->vm_pgoff += adjust_next;
 	}
-
-    if (unlikely(waydroid_mmap_dbg_task())) {
-	pr_emerg("VMAADJ: fields-updated comm=%s pid=%d vma=%p now=%lx-%lx next=%p next_now=%lx-%lx adjust_next=%ld remove_next=%d\n",
-	     current->comm, current->pid,
-	     vma,
-	     vma->vm_start, vma->vm_end,
-	     next,
-	     next ? next->vm_start : 0UL,
-	     next ? next->vm_end : 0UL,
-	     adjust_next, remove_next);
-
-	if (unlikely(vma->vm_start > vma->vm_end ||
-	         (next && next->vm_start > next->vm_end))) {
-	    pr_emerg("VMAADJ-BAD-AFTER: vma=%p now=%lx-%lx next=%p next_now=%lx-%lx adjust_next=%ld remove_next=%d\n",
-		 vma,
-		 vma->vm_start, vma->vm_end,
-		 next,
-		 next ? next->vm_start : 0UL,
-		 next ? next->vm_end : 0UL,
-		 adjust_next, remove_next);
-	    dump_stack();
-	    BUG();
-	}
-    }
 
 	if (root) {
 		if (adjust_next)
@@ -883,44 +814,12 @@ struct vm_area_struct *vma_merge(struct mm_struct *mm,
  			mpol_equal(policy, vma_policy(next)) &&
 			can_vma_merge_before(next, vm_flags, anon_vma,
 					file, pgoff+pglen, anon_name)) {
-
-	if (prev && addr < prev->vm_end) { /* case 4 */
-	    if (unlikely(waydroid_mmap_dbg_task())) {
-		pr_emerg("VMAMERGE: case4 comm=%s pid=%d prev=%p prev_range=%lx-%lx addr=%lx end=%lx area=%p next=%p next_range=%lx-%lx\n",
-		     current->comm, current->pid,
-		     prev,
-		     prev->vm_start, prev->vm_end,
-		     addr, end,
-		     area, next,
-		     next ? next->vm_start : 0UL,
-		     next ? next->vm_end : 0UL);
-
-		if (unlikely(addr < prev->vm_start)) {
-		    pr_emerg("VMAMERGE-BAD-CASE4: addr=%lx is below prev->vm_start=%lx; refusing to create inverted VMA\n",
-			 addr, prev->vm_start);
-		    dump_stack();
-		    BUG();
-		}
-	    }
-
-	    err = vma_adjust(prev, prev->vm_start,
-		     addr, prev->vm_pgoff, NULL);
-	} else { /* cases 3, 8 */
-	    if (unlikely(waydroid_mmap_dbg_task()))
-		pr_emerg("VMAMERGE: case3or8 comm=%s pid=%d area=%p area_range=%lx-%lx addr=%lx end=%lx next=%p next_range=%lx-%lx\n",
-		     current->comm, current->pid,
-		     area,
-		     area ? area->vm_start : 0UL,
-		     area ? area->vm_end : 0UL,
-		     addr, end,
-		     next,
-		     next ? next->vm_start : 0UL,
-		     next ? next->vm_end : 0UL);
-
-	    err = vma_adjust(area, addr, next->vm_end,
-		     next->vm_pgoff - pglen, NULL);
-	}
-
+		if (prev && addr < prev->vm_end)	/* case 4 */
+			err = vma_adjust(prev, prev->vm_start,
+				addr, prev->vm_pgoff, NULL);
+		else					/* cases 3, 8 */
+			err = vma_adjust(area, addr, next->vm_end,
+				next->vm_pgoff - pglen, NULL);
 		if (err)
 			return NULL;
 		khugepaged_enter_vma_merge(area);
@@ -1194,14 +1093,6 @@ static unsigned long do_mmap_pgoff(struct file *file, unsigned long addr,
 	if (error)
 		return error;
 
-    if (unlikely(waydroid_mmap_dbg_task())) {
-	pr_emerg("WMMAP: do_mmap_pgoff comm=%s pid=%d file=%s addr=%lx len=%lx prot=%lx flags=%lx vm_flags=%lx pgoff=%lx map_count=%d\n",
-	     current->comm, task_pid_nr(current),
-	     waydroid_mmap_file_name(file),
-	     addr, len, prot, flags, vm_flags, pgoff,
-	     current->mm ? current->mm->map_count : -1);
-    }
-
 	return mmap_region(file, addr, len, flags, vm_flags, pgoff);
 }
 
@@ -1470,29 +1361,6 @@ munmap_back:
 		if (pgprot_val(pprot) == pgprot_val(pgprot_noncached(pprot)))
 			vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
 	}
-
-    if (unlikely(waydroid_mmap_dbg_task())) {
-	pr_emerg("WMMAP: before vma_link comm=%s pid=%d vma=%p start=%lx end=%lx flags=%lx pgoff=%lx file=%s\n",
-	     current->comm, task_pid_nr(current),
-	     vma, vma->vm_start, vma->vm_end,
-	     vma->vm_flags, vma->vm_pgoff,
-	     waydroid_mmap_file_name(vma->vm_file));
-
-	pr_emerg("WMMAP: prev=%p rb_link=%p rb_link_val=%p rb_parent=%p root=%p rootnode=%p map_count=%d\n",
-	     prev, rb_link, rb_link ? *rb_link : NULL,
-	     rb_parent, &mm->mm_rb, mm->mm_rb.rb_node,
-	     mm->map_count);
-
-	if (prev)
-	    pr_emerg("WMMAP: prev range=%lx-%lx flags=%lx file=%s\n",
-		 prev->vm_start, prev->vm_end, prev->vm_flags,
-		 waydroid_mmap_file_name(prev->vm_file));
-
-	if (rb_parent)
-	    pr_emerg("WMMAP: rb_parent left=%p right=%p parent_color=%lx\n",
-		 rb_parent->rb_left, rb_parent->rb_right,
-		 rb_parent->rb_parent_color);
-    }
 
 	vma_link(mm, vma, prev, rb_link, rb_parent);
 	file = vma->vm_file;
